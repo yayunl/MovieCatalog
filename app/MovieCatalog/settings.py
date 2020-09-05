db58dd9ff6bdaf705653eda356073c31a11ef8d9
krashilili90 <krashilili0915@gmail.com>
2020-09-05 14:56:58 -0500

    Add postgres and pgadmin 4.

--- app/MovieCatalog/settings.py
+++ app/MovieCatalog/settings.py
@@ -80,9 +80,13 @@ WSGI_APPLICATION = 'MovieCatalog.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+     "default": {
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
+        "USER": os.environ.get("SQL_USER", "admin"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+        "HOST": os.environ.get("SQL_HOST", "localhost"),
+        "PORT": os.environ.get("SQL_PORT", "5432"),
     }
 }
 
